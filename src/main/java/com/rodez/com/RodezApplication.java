5faa51f8545d5d36ae7016eddca6b0755e6ee9c8
Delhumeur <126495668+DervauxJulien@users.noreply.github.com>
2024-11-27 16:36:05 +0100

    first commit

---
+++ src/main/java/com/rodez/com/RodezApplication.java
@@ -0,0 +1,13 @@
+package com.rodez.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodez/com/RodezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodez.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
