dc901f1b48c9bd19d152ae8b3158b11434008fbb
khierdevja <khierdevja@gmail.com>
2024-12-10 10:14:54 +0100

    Modif de la validation de l'utilisateur en base de donnees, ainsi que le login pour afficher le dashboard

--- src/main/java/com/rodez/com/Controller/api/InterventionRestController.java
+++ src/main/java/com/rodez/com/Controller/api/InterventionRestController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -33,12 +35,16 @@ public class InterventionRestController {
         return interventionService.getById(id);
     }
 
-    @GetMapping("/checkUser")
+    @PostMapping("/checkUser")
     public Integer connect(@RequestBody User user){
         Integer check = 0;
-        User userDatabase = userService.getByRegistration(user.getRegistration());
-        if(userDatabase != null){
-            check = userDatabase.getIdUser();
+        Iterable<User> users = userService.listAll();
+        List<User> userList = new ArrayList<User>();
+        users.forEach(userList::add);
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getRegistration().equals(user.getRegistration()) && userList.get(i).getLastname().equals(user.getLastname()) && userList.get(i).getFirstname().equals(user.getFirstname())) {
+                check = userList.get(i).getIdUser();
+            }
         }
         return check;
     }
@@ -58,16 +64,23 @@ public class InterventionRestController {
 
     @PostMapping("/login")
     public Iterable<Intervention> login(@RequestBody User user){
-        Integer idUser = connect(user);
+        Integer idUser = 0;
+        Iterable<User> users = userService.listAll();
+        List<User> userList = new ArrayList<User>();
+        users.forEach(userList::add);
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getRegistration().equals(user.getRegistration()) && userList.get(i).getPasswordUser().equals(user.getPasswordUser())) {
+                idUser = userList.get(i).getIdUser();
+            }
+        }
+
         Iterable<Intervention> listInterventions = null;
         if(idUser!= 0){
-            if(user.getPasswordUser() == userService.getUserById(idUser).get().getPasswordUser()){
-                if(user.getRegistration().charAt(0) == 'A' || user.getRegistration().charAt(0) == 'F'){
-                    listInterventions = interventionService.getAll();
-                }
-                else{
-                    listInterventions = interventionService.getMyRequest(idUser);
-                }
+            if(user.getRegistration().charAt(0) == 'A' || user.getRegistration().charAt(0) == 'F'){
+                listInterventions = interventionService.getAll();
+            }
+            else{
+                listInterventions = interventionService.getMyRequest(idUser);
             }
         }
         return listInterventions;
