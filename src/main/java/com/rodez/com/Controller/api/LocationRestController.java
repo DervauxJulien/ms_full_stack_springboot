68936ecbe2bdc56dbea1bae29f6ec74ab76b7329
khierdevja <khierdevja@gmail.com>
2024-12-11 14:18:46 +0100

    GetMapping en PostMapping

--- src/main/java/com/rodez/com/Controller/api/LocationRestController.java
+++ src/main/java/com/rodez/com/Controller/api/LocationRestController.java
@@ -7,6 +7,7 @@ import com.rodez.com.Service.LocationService;
 import com.rodez.com.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +16,7 @@ public class LocationRestController {
     @Autowired
     private LocationService locationService = new LocationService();
 
-    @GetMapping("locations_admin_rest")
+    @PostMapping("locations_admin_rest")
     public Iterable <Location> getAllLocations(){
         return locationService.getLocation();
 
